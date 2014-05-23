ac5268668df779010633d4f62279edbcdc56ba4e
Espartaco Palma <esparta@gmail.com>
2014-05-23 10:21:23 -0500

    Don't let add more than one metro

--- src/static/js/metro.js
+++ src/static/js/metro.js
@@ -1,13 +1,12 @@
-var metros = [];
+var metros = {};
 var total = 380;
 var disp  = total;
-
-
+console.log("Metros", metros);
 
 function search(term) {
     var logos = document.getElementsByName('logo');
     for(var i = 0; i < logos.length; i++) {
-        logo = logos[i];
+        var logo = logos[i];
         if(logo.title.toLowerCase().indexOf(term.toLowerCase()) > -1) {
             logo.style.display = 'inline';
             disp++;
@@ -23,37 +22,40 @@ function setTitle(text) {
     document.getElementById('title').innerHTML = text;
 }
 
+function getId(uri){
+    uri = uri.substring(uri.lastIndexOf("/")+1);
+    uri = uri.substring(0, uri.length - 4);
+    return uri;
+}
+
 function add(logo) {
-    metros.push(logo.src);
     document.getElementById('instructions').style.display = 'none';
-    var img = document.createElement("img");
-    img.src = logo.src;
-    img.title = logo.title;
-    img.onclick = function() {
-        this.style.display = "none";
-        metros.splice(metros.indexOf(this.src, 1));
+    var id = getId(logo.src);
+    if (!metros.hasOwnProperty(id)){
+        metros[id] = logo.src;
+        var img = document.createElement("img");
+        img.src = logo.src;
+        img.title = logo.title;
+        img.id = id;
+        img.onclick = function() {
+            this.style.display = "none";
+            delete metros[this.id];
+        };
+        img.onmouseover = function() {
+            setTitle(this.title);
+        };
+        img.onmouseout = function() {
+            setTitle('');
+        };
+        document.getElementById('result').appendChild(img);
     }
-    img.onmouseover = function() {
-        setTitle(this.title);
-    }
-    img.onmouseout = function() {
-        setTitle('');
-    }
-    document.getElementById('result').appendChild(img);
 }
 
 function generate() {
-    var logos = [];
-    for(var i = 0; i < metros.length; i++) {
-        var logo = metros[i];
-        logo = logo.substring(logo.lastIndexOf("/")+1);
-        logo = logo.substring(0, logo.length - 4);
-        logos.push(logo);
-    }
-    window.location.href =  "index.php?logos=" + logos.join(",");
+    window.location.href =  "index.php?logos=" + Object.keys(metros).join(",");
 }
 
 function clearAll() {
     document.getElementById('result').innerHTML = '';
-    metros = [];
+    metros = {};
 }
